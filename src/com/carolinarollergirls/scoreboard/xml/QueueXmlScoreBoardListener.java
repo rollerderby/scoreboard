84f181bb3e3dab4230dda8c04f53f88591a638a8
ddstreet <ddstreet@users.sourceforge.net>
2011-02-09 17:33:28 -0500

    Update class to optionally (and by default) merge new documents unless a new doc has an element with the "remove" attribute, and if so start a new queue element with the next document

--- src/com/carolinarollergirls/scoreboard/xml/QueueXmlScoreBoardListener.java
+++ src/com/carolinarollergirls/scoreboard/xml/QueueXmlScoreBoardListener.java
@@ -13,14 +13,31 @@ public class QueueXmlScoreBoardListener implements XmlScoreBoardListener
 	}
 
 	public void xmlChange(Document d) {
-		documents.offer(d);
+		synchronized (documentsLock) {
+			if (queueNextDocument || documents.isEmpty())
+				documents.addLast(d);
+			else
+				editor.mergeDocuments(documents.getLast(), d);
+
+			queueNextDocument = (isQueueOnlyRemovals() && editor.hasElementRemoval(d));
+		}
 	}
 
 	public Document getDocument() {
-		return documents.poll();
+		synchronized (documentsLock) {
+			return documents.poll();
+		}
 	}
 
 	public boolean isEmpty() { return (null != documents.peek()); }
 
-	protected Queue<Document> documents = new ConcurrentLinkedQueue<Document>();
+	public boolean isQueueOnlyRemovals() { return queueOnlyRemovals; }
+	public void setQueueOnlyRemovals(boolean q) { queueOnlyRemovals = q; }
+
+	protected XmlDocumentEditor editor = new XmlDocumentEditor();
+
+	protected boolean queueOnlyRemovals = true;
+	protected boolean queueNextDocument = false;
+	protected LinkedList<Document> documents = new LinkedList<Document>();
+	protected Object documentsLock = new Object();
 }
