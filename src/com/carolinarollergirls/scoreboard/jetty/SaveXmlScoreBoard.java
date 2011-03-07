db74aa2ae0eee3986253ed758702a9ff5ed6c6a5
ddstreet <ddstreet@users.sourceforge.net>
2011-03-07 18:55:04 -0500

    update path handling

--- src/com/carolinarollergirls/scoreboard/jetty/SaveXmlScoreBoard.java
+++ src/com/carolinarollergirls/scoreboard/jetty/SaveXmlScoreBoard.java
@@ -2,6 +2,7 @@ package com.carolinarollergirls.scoreboard.jetty;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.*;
 
 import org.jdom.*;
 import org.jdom.xpath.*;
@@ -27,12 +28,46 @@ public class SaveXmlScoreBoard extends DefaultScoreBoardControllerServlet
 
 		Element node = getXmlScoreBoard().getDocument().getRootElement();
 
+		/* If any path is specified, this attempts to match the
+		 * element(s) for the specified path and return a single
+		 * document with all matched elements and all their
+		 * descendent elements.  Associated attributes, text, etc.
+		 * for all returned elements are preserved.
+		 *
+		 * The path format should be either:
+		 *   ScoreBoard Javascript, e.g. ScoreBoard.Team(1).Name
+		 *   xpath, e.g. ScoreBoard/Team[@Id='1']/Name
+		 * As an exception, ScoreBoard Javascript format may
+		 * be used with forward slashes instead of periods, e.g.
+		 * ScoreBoard/Team(1)/Name
+		 *
+		 * If a Id attribute is omitted for an element with an Id,
+		 * all elements with that name will be included in the results.
+		 *
+		 * examples:
+		 * ScoreBoard.Team(1).Name
+		 * ScoreBoard/Team(1)/Name
+		 * ScoreBoard/Team[@Id='1']/Name
+		 * ScoreBoard/Team/Name
+		 *
+		 * Note the first 3 examples will match only one element, while the
+		 * last example will match the Name element under all/both Team elements.
+		 */
 		String path = request.getPathInfo();
 		if (path.startsWith("/"))
 			path = path.substring(1);
-		path = path.replaceAll("\\(", "[@Id='").replaceAll("\\)", "']");
-		try {
-			if (path.length() > 0) {
+		StringBuffer buffer = new StringBuffer();
+		Matcher matcher = Pattern.compile("^[^()]+?(\\(.*?\\))?[./]").matcher(path);
+		while (matcher.find()) {
+			buffer.append(matcher.group().replaceFirst("[.]$", "/"));
+			matcher.region(matcher.end(), matcher.regionEnd());
+		}
+		buffer.append(path.substring(matcher.regionStart()));
+		path = buffer.toString().replaceAll("\\((.*?)\\)", "[@Id='$1']");
+		if (path.endsWith("/"))
+			path = path.substring(0, path.length()-1);
+		if (path.length() > 0) {
+			try {
 				Iterator nodes = XPath.selectNodes(node, path).iterator();
 				node = null;
 				while (nodes.hasNext()) {
@@ -42,9 +77,9 @@ public class SaveXmlScoreBoard extends DefaultScoreBoardControllerServlet
 					else
 						editor.mergeDocuments(node.getDocument(), n.getDocument());
 				}
+			} catch ( Exception e ) {
+				node = null;
 			}
-		} catch ( Exception e ) {
-			node = null;
 		}
 
 		if (node == null) {
