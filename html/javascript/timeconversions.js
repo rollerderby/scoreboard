dda92f15c3a784b1ac83aa9d6b778c6d4081dfc1
ddstreet <ddstreet@users.sourceforge.net>
2011-03-15 11:09:09 -0400

    add twoDigit() function rearrange ordering of functions and add comments

--- html/javascript/timeconversions.js
+++ html/javascript/timeconversions.js
@@ -1,20 +1,10 @@
 
 
 _timeConversions = {
-	/* If needed, this preprends ":" */
-	_formatMinSec: function(time) { return ((String(time).indexOf(":") > -1) ? time : ":"+time); },
-	/* Convert ms to seconds portion of min:sec */
-	_msOnlySec: function(ms) {
-		var s = (_timeConversions.msToSeconds(ms) % 60);
-		if (10 > s) s = "0"+s;
-		return String(s);
-	},
-	/* Convert ms to minutes portion of min:sec */
-	_msOnlyMin: function(ms) { return String(Math.floor(ms / 60000)); },
-	/* Accepts min:sec time, returns only seconds portion */
-	_timeOnlySec: function(time) { return _timeConversions._formatMinSec(time).split(":")[1]; },
-	/* Accepts min:sec time, returns only minutes portion */
-	_timeOnlyMin: function(time) { return _timeConversions._formatMinSec(time).split(":")[0]; },
+	/*
+	 * These are conversion functions for external use
+	 */
+
 	/* Convert ms to seconds */
 	msToSeconds: function(ms) { return String(Math.floor(ms / 1000)); },
 	/* Convert seconds to milliseconds */
@@ -32,5 +22,27 @@ _timeConversions = {
 		return ((ms >= 60000) ? _timeConversions.msToMinSec(ms) : _timeConversions.msToSeconds(ms));
 	},
 	/* Same as minSecToMs() */
-	minSecNoZeroToMs: function(time) { return _timeConversions.minSecToMs(time); }
+	minSecNoZeroToMs: function(time) { return _timeConversions.minSecToMs(time); },
+
+	/*
+	 * These are utility functions that can be used externally
+	 */
+
+	/* Format number into minimum 2 digits, prepending 0 if needed */
+	twoDigit: function(n) { return (10 > n ? "0" : "")+n; },
+
+	/*
+	 * These are generally internal functions that should not be used externally
+	 */
+
+	/* If needed, this preprends ":" */
+	_formatMinSec: function(time) { return ((String(time).indexOf(":") > -1) ? time : ":"+time); },
+	/* Convert ms to seconds portion of min:sec */
+	_msOnlySec: function(ms) { return _timeConversions.twoDigit(_timeConversions.msToSeconds(ms) % 60); },
+	/* Convert ms to minutes portion of min:sec */
+	_msOnlyMin: function(ms) { return String(Math.floor(ms / 60000)); },
+	/* Accepts min:sec time, returns only seconds portion */
+	_timeOnlySec: function(time) { return _timeConversions._formatMinSec(time).split(":")[1]; },
+	/* Accepts min:sec time, returns only minutes portion */
+	_timeOnlyMin: function(time) { return _timeConversions._formatMinSec(time).split(":")[0]; }
 };
