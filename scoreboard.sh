4f8190e36d3a52f57adfcc82a13442bf513c46f8
ddstreet <ddstreet@users.sourceforge.net>
2011-04-01 23:56:44 -0400

    add check for interactive shell, only run gui in non-interactive shell

--- scoreboard.sh
+++ scoreboard.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-exec java -Xmx256m -jar lib/crg-scoreboard.jar --gui
+GUI="--gui"
 
+# If fd 0 (stdin) exists, this is an interactive shell, so don't use the gui
+test -t "0" && GUI=""
 
+exec java -Xmx256m -jar lib/crg-scoreboard.jar $GUI
