45d54866ab82cdbe6eb23166bebc2628ec299780
ddstreet <ddstreet@users.sourceforge.net>
2011-04-22 18:29:06 -0400

    remove exec from java call

--- scoreboard-Linux.sh
+++ scoreboard-Linux.sh
@@ -7,4 +7,4 @@ GUI="--gui"
 # If fd 0 (stdin) exists, this is an interactive shell, so don't use the gui
 test -t "0" && GUI=""
 
-exec java -Xmx256m -jar lib/crg-scoreboard.jar $GUI
+java -Xmx256m -jar lib/crg-scoreboard.jar $GUI
