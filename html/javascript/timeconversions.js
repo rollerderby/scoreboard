3ab89d29dc706ebe95d183e85b7c1d09e1705af0
ddstreet <ddstreet@users.sourceforge.net>
2011-11-01 15:09:08 -0400

    Update timeconversions javascript lib to allow rounding ms up or down; change default to round up note: need to add functionality somehow to allow rounding depending on clock direction

--- html/javascript/timeconversions.js
+++ html/javascript/timeconversions.js
@@ -1,12 +1,21 @@
 
 
+//FIXME - need to set policy based on direction of clock...
 _timeConversions = {
+  /* Policy: round ms up or down
+   * If true, ms values will be rounded up, i.e. 1001-2000 ms converts to 2 sec.
+   * If false, ms values will be rounded down, i.e. 1000-1999 ms converts to 1 sec.
+   */
+  roundUp: true,
+
   /*
    * These are conversion functions for external use
    */
 
   /* Convert ms to seconds */
-  msToSeconds: function(ms) { return String(Math.floor(ms / 1000)); },
+  msToSeconds: function(ms) {
+    return String(_timeConversions.roundUp ? Math.ceil(ms / 1000) : Math.floor(ms / 1000));
+  },
   /* Convert seconds to milliseconds */
   secondsToMs: function(sec) { return String(sec * 1000); },
   /* Convert milliseconds to min:sec */
@@ -15,11 +24,12 @@ _timeConversions = {
   minSecToMs: function(time) {
     var min = Number(_timeConversions._timeOnlyMin(time) || 0);
     var sec = Number(_timeConversions._timeOnlySec(time) || 0);
-    return String(((min * 60) + sec) * 1000);
+    return _timeConversions.secondsToMs((min * 60) + sec);
   },
   /* Convert milliseconds to min:sec, if min is 0 return only seconds portion */
   msToMinSecNoZero: function(ms) {
-    return ((ms >= 60000) ? _timeConversions.msToMinSec(ms) : _timeConversions.msToSeconds(ms));
+    var limit = (_timeConversions.roundUp ? 59001 : 60000);
+    return ((ms >= limit) ? _timeConversions.msToMinSec(ms) : _timeConversions.msToSeconds(ms));
   },
   /* Same as minSecToMs() */
   minSecNoZeroToMs: function(time) { return _timeConversions.minSecToMs(time); },
@@ -40,7 +50,11 @@ _timeConversions = {
   /* Convert ms to seconds portion of min:sec */
   _msOnlySec: function(ms) { return _timeConversions.twoDigit(_timeConversions.msToSeconds(ms) % 60); },
   /* Convert ms to minutes portion of min:sec */
-  _msOnlyMin: function(ms) { return String(Math.floor(ms / 60000)); },
+  _msOnlyMin: function(ms) {
+    var min = Math.floor(ms / 60000);
+    min = min + Number(_timeConversions.msToSeconds(Math.max(0, (ms % 60000) - 59000)));
+    return String(min);
+  },
   /* Accepts min:sec time, returns only seconds portion */
   _timeOnlySec: function(time) { return _timeConversions._formatMinSec(time).split(":")[1]; },
   /* Accepts min:sec time, returns only minutes portion */
