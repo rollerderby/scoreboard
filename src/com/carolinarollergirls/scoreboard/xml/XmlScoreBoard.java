ae7dc1616e8798102e735dcf0b5a989fb4a8f01e
ddstreet <ddstreet@users.sourceforge.net>
2011-11-04 16:29:10 -0400

    re-enable xml stats collection

--- src/com/carolinarollergirls/scoreboard/xml/XmlScoreBoard.java
+++ src/com/carolinarollergirls/scoreboard/xml/XmlScoreBoard.java
@@ -162,8 +162,8 @@ public class XmlScoreBoard
   protected void loadXmlDocumentManagers() {
 //FIXME - this isn't the right way to do this!  use properties file, or xml maybe?
     // disable these until they are useful/complete.
-    //new XmlRealtimeStats().setXmlScoreBoard(this);
-    //new XmlInterpretedStats().setXmlScoreBoard(this);
+    new XmlRealtimeStats().setXmlScoreBoard(this);
+    new XmlInterpretedStats().setXmlScoreBoard(this);
     //new XmlGoogleDocsStats().setXmlScoreBoard(this);
     new LoadScoreBoard().setXmlScoreBoard(this);
     new TeamsXmlDocumentManager().setXmlScoreBoard(this);
