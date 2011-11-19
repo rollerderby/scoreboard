23cf35736ac54aaa02a446b7aeff58a8420af653
ddstreet <ddstreet@users.sourceforge.net>
2011-11-18 22:35:26 -0500

    remove countdown clock references and policy

--- src/com/carolinarollergirls/scoreboard/Clock.java
+++ src/com/carolinarollergirls/scoreboard/Clock.java
@@ -27,13 +27,4 @@ public interface Clock extends ScoreBoardEventProvider
   public static final String ID_LINEUP = "Lineup";
   public static final String ID_TIMEOUT = "Timeout";
   public static final String ID_INTERMISSION = "Intermission";
-  public static final String ID_COUNTDOWN = "Countdown";
-  public static final String ID_PENALTY_TEAM1_B1 = "PenaltyTeam1B1";
-  public static final String ID_PENALTY_TEAM1_B2 = "PenaltyTeam1B2";
-  public static final String ID_PENALTY_TEAM1_B3 = "PenaltyTeam1B3";
-  public static final String ID_PENALTY_TEAM1_J = "PenaltyTeam1J";
-  public static final String ID_PENALTY_TEAM2_B1 = "PenaltyTeam2B1";
-  public static final String ID_PENALTY_TEAM2_B2 = "PenaltyTeam2B2";
-  public static final String ID_PENALTY_TEAM2_B3 = "PenaltyTeam2B3";
-  public static final String ID_PENALTY_TEAM2_J = "PenaltyTeam2J";
 }
