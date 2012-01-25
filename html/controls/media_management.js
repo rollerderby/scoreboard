fe4ea8cbbdae4e77558b332c5cc4a8f8e780cb86
ddstreet <ddstreet@users.sourceforge.net>
2012-01-25 16:37:27 -0500

    Add type-specific add popup to media_management page add list-media servlet to show all media files in each media type directory

--- html/controls/media_management.js
+++ html/controls/media_management.js
@@ -2,9 +2,9 @@
 
 $sb(function() {
   $("body>table.TypeTemplate")
-    .find("tr.Type>th.Type>button").click(function() {
+    .find("tr.Type>th.Type>button").filter(".Show,.Hide").click(function() {
       $(this).closest("table").toggleClass("Hide");
-    }).button().end()
+    }).end().button().end()
     .find("tr.Controls>th.Remove>input:checkbox.Remove").click(function() {
       $(this).closest("table.Type").find("tr.Item>td.Remove>input:checkbox")
         .prop("checked", this.checked);
@@ -31,9 +31,9 @@ $sb(function() {
   setupTab("Videos", "Video", "<video>");
   setupTab("CustomHtml", "Html", "<iframe>");
 
-  var imagesDialog = createDialog("Images", "Image");
-  var videosDialog = createDialog("Videos", "Video");
-  var customHtmlDialog = createDialog("CustomHtml", "Html");
+  var imagesDialog = createAddSpecificDialog("Images", "Image");
+  var videosDialog = createAddSpecificDialog("Videos", "Video");
+  var customHtmlDialog = createAddSpecificDialog("CustomHtml", "Html");
 
   $("#Images>div.Buttons>button.Add").button()
     .click(function() { imagesDialog.dialog("open"); });
@@ -67,6 +67,9 @@ function setupTab(parentName, childName, previewElement) {
   $sb(parentName).$sbBindAddRemoveEach("Type", function(event,node) {
     var newTable = $("body>table.TypeTemplate").clone(true)
       .removeClass("TypeTemplate").addClass("Type").data("Type", node.$sbId).data("sb", node)
+      .find("tr.Type>th.Type>button.Add").click(function() {
+        createAddTypeDialog(parentName, node.$sbId, childName);
+      }).end()
       .find("tr.Type>th.Type>a.Type>span.Type").html(node.$sbId).end();
     _windowFunctions.appendSorted($("#"+parentName+">div.Type"), newTable, compareType);
   }, function(event,node) {
@@ -113,13 +116,67 @@ function setupTab(parentName, childName, previewElement) {
   });
 }
 
-function createDialog(parentName, childName) {
+function createAddTypeDialog(parentName, type, childName) {
+  var media = parentName.toLowerCase();
+  var sbType = $sb(parentName+".Type("+type+")");
+  var div = $("body>div.AddTypeDialogTemplate").clone(true)
+    .removeClass("AddTypeDialogTemplate").addClass("AddTypeDialog");
+  var listTable = div.children("table.List");
+  var listTbody = listTable.children("tbody");
+  var statusTd = listTbody.find("tr.Status>td");
+  var addFiles = function() {
+    alert("implement add files");
+    div.dialog("close");
+  };
+  var refreshList = function() {
+    $.get("/listmedia", { media: media, type: type })
+      .fail(function() { statusTd.text("Error loading media list").show(); })
+      .done(function(data, status, jqxhr) {
+        $.each(String(jqxhr.responseText.trim()).split("\n"), function(i,e) {
+          var name = e.replace(/\.[^.]*$/, "");
+          var source = "/"+media+"/"+type+"/"+e;
+          var exists = sbType.children(childName).children("Src:contains("+source+")").length;
+          var row = $("<tr>").addClass("Media")
+            .append($("<td>").addClass("Add"))
+            .append($("<td>").addClass("Source"))
+            .data("source", e)
+            .addClass(exists?"Exists":"New");
+          $("<a>").text("Added").hide().appendTo(row.children("td.Add"));
+          $("<button>").text("Add").button().click(function() {
+            var newElem = $sb(parentName+".Type("+type+")."+childName+"("+name+")");
+            newElem.$sb("Name").$sbSet(name);
+            newElem.$sb("Src").$sbSet(source);
+            $(this).hide().prev("a").show();
+          }).appendTo(row.children("td.Add"));
+          row.children("td.Source").text(e);
+          _windowFunctions.appendAlphaSortedByData(listTbody, row, "source", 1);
+        });
+        statusTd.hide();
+      })
+      .always(function() { div.children("button.Refresh").button("enable"); });
+  };
+  div.children("button.Refresh").button().click(function() {
+    $(this).button("disable");
+    listTbody.children("tr.Media").remove();
+    statusTd.text("Loading...").show();
+    refreshList();
+  }).click();
+  div.dialog({
+    title: "Add '"+type+"' media",
+    modal: true,
+    width: "700px",
+    close: function() { div.dialog("destroy").remove(); },
+    buttons: { Close: function() { div.dialog("close"); } }
+  });
+}
+
+function createAddSpecificDialog(parentName, childName) {
   var close = function() {
     $(this).find("input:text").val("");
     $(this).dialog("close");
   };
-  return $("body>div.AddDialogTemplate").clone(true)
-    .removeClass("AddDialogTemplate").addClass("AddDialog "+parentName+"Dialog")
+  return $("body>div.AddSpecificDialogTemplate").clone(true)
+    .removeClass("AddSpecificDialogTemplate").addClass("AddSpecificDialog "+parentName+"Dialog")
     .dialog({
       modal: true,
       autoOpen: false,
