b1c65b8eea7f5cfa2f5ca1c550abb01d78cd3d53
ddstreet <ddstreet@users.sourceforge.net>
2012-02-15 16:02:54 -0500

    Add copyright header to all files

--- html/javascript/core.js
+++ html/javascript/core.js
@@ -1,4 +1,13 @@
 
+/**
+ * Copyright (C) 2008-2012 Mr Temper <MrTemper@CarolinaRollergirls.com>
+ *
+ * This file is part of the Carolina Rollergirls (CRG) ScoreBoard.
+ * The CRG ScoreBoard is licensed under either the GNU General Public
+ * License version 3 (or later), or the Apache License 2.0, at your option.
+ * See the file COPYING for details.
+ */
+
 /*
  * The main scoreboard API is the $sb() function.  Its use can be:
  *   $sb(function)
--- src/com/carolinarollergirls/scoreboard/xml/OpenXmlDocumentManager.java
+++ src/com/carolinarollergirls/scoreboard/xml/OpenXmlDocumentManager.java
@@ -1,4 +1,12 @@
 package com.carolinarollergirls.scoreboard.xml;
+/**
+ * Copyright (C) 2008-2012 Mr Temper <MrTemper@CarolinaRollergirls.com>
+ *
+ * This file is part of the Carolina Rollergirls (CRG) ScoreBoard.
+ * The CRG ScoreBoard is licensed under either the GNU General Public
+ * License version 3 (or later), or the Apache License 2.0, at your option.
+ * See the file COPYING for details.
+ */
 
 import java.util.*;
 
