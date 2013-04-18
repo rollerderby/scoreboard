97992452c9d8b5847d2f8b2903ee6e7b511b61fd
ddstreet <ddstreet@users.sourceforge.net>
2013-04-17 23:20:52 -0400

    When merging or loading a saved xml file into the sb, do not clear out the input:file element immediately after form.submit(), as it seems the actual submission is delayed in some cases, causing a failure when there is no file to upload.

--- html/controls/operator.js
+++ html/controls/operator.js
@@ -1320,8 +1320,7 @@ function createSaveLoadContent() {
   $("<button>").html("Add/Merge").attr("data-method", "merge").appendTo(uploadForm).button();
   $("<button>").html("Replace running scoreboard").attr("data-method", "load").appendTo(uploadForm).button();
   uploadForm.children("button").click(function() {
-    uploadForm.attr("action", "/LoadXml/"+$(this).attr("data-method")).submit()
-      .children("input:file").val("").change();
+    uploadForm.attr("action", "/LoadXml/"+$(this).attr("data-method")).submit();
   });
   _crgUtils.bindAndRun(uploadForm.children("input:file").button(), "change", function() {
     uploadForm.children("button").button(this.value ? "enable" : "disable");
