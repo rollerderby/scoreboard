8a29fb10c07c3aee8578c04e720cb2febedd018c
ddstreet <ddstreet@ieee.org>
2013-04-25 10:24:29 -0400

    update MediaXmlDocumentManager to reject any external changes - its tree strictly reflects the actual on-disc files now update MediaServlet to remove unneeded code after above change (no need to list on-disc files separately from XML doc now, or manage the XML separately from the files on-disc), and add code to pull single media file from remote server, for initial map-based media management on remote (centralized) server

--- src/com/carolinarollergirls/scoreboard/xml/MediaXmlDocumentManager.java
+++ src/com/carolinarollergirls/scoreboard/xml/MediaXmlDocumentManager.java
@@ -14,6 +14,9 @@ import java.io.*;
 import org.jdom.*;
 import org.jdom.xpath.*;
 
+import org.apache.commons.io.monitor.*;
+import org.apache.commons.io.filefilter.*;
+
 import com.carolinarollergirls.scoreboard.*;
 import com.carolinarollergirls.scoreboard.jetty.*;
 import com.carolinarollergirls.scoreboard.xml.*;
@@ -26,34 +29,86 @@ public class MediaXmlDocumentManager extends OpenXmlDocumentManager implements X
   }
 
   public void reset() {
-    super.reset();
-    loadTypes();
-  }
-
-  protected void loadTypes() {
-    FilenameFilter managedFilter = new FilenameFilter() {
-        public boolean accept(File dir, String name) {
-          return getManagedElementName().equalsIgnoreCase(name);
+    synchronized (typeMonitors) {
+      Iterator<String> types = typeMonitors.keySet().iterator();
+      while (types.hasNext()) {
+        String type = types.next();
+        try {
+          typeMonitors.get(type).stop();
+        } catch (Exception e) {
+          ScoreBoardManager.printMessage("MediaXmlDocumentManager for "+getMediaName()+" ERROR: Could not stop type "+type+" monitor : "+e.getMessage());
         }
-      };
-    FileFilter typeFilter = new FileFilter() {
-        public boolean accept(File dir) {
-          return (dir.isDirectory());
-        }
-      };
-    File topDir = new File(htmlDirName);
-    Iterator<File> managedDirs = Arrays.asList(topDir.listFiles(managedFilter)).iterator();
-    while (managedDirs.hasNext()) {
-      File managedDir = managedDirs.next();
-      Iterator<File> typeDirs = Arrays.asList(managedDir.listFiles(typeFilter)).iterator();
-      while (typeDirs.hasNext())
-        update(editor.getElement(createXPathElement(), "Type", typeDirs.next().getName()));
+      }
+      typeMonitors.clear();
     }
+
+    super.reset();
+    monitorTypes();
   }
 
+  protected void processChildElement(Element e) throws Exception {
+    /* ONLY process reset - our tree strictly reflects the actual media files on disc */
+    if (e.getName().equals("Reset"))
+      super.processChildElement(e);
+  }
+
+  protected void monitorTypes() {
+    File managedDir = new File(htmlDirName, getManagedDirName());
+    Iterator<String> types = Arrays.asList(managedDir.list(DirectoryFileFilter.INSTANCE)).iterator();
+    while (types.hasNext())
+      monitorType(managedDir, types.next());
+  }
+
+  protected void monitorType(File managedDir, String type) {
+    File typeDir = new File(managedDir, type);
+    update(editor.getElement(createXPathElement(), "Type", type));
+    FileAlterationObserver faO = new FileAlterationObserver(typeDir, mediaFileFilter);
+    faO.addListener(new MediaTypeDirAlterationListener(type));
+    FileAlterationMonitor monitor = new FileAlterationMonitor(INTERVAL, faO);
+    try {
+      monitor.start();
+    } catch (Exception e) {
+      ScoreBoardManager.printMessage("MediaXmlDocumentManager for "+getMediaName()+" ERROR: Could not start type "+type+" monitor : "+e.getMessage());
+    }
+    typeMonitors.put(type, monitor);
+    Iterator<File> files = Arrays.asList(typeDir.listFiles((FileFilter)mediaFileFilter)).iterator();
+    while (files.hasNext())
+      addMediaElement(type, files.next());
+  }
+
+  protected String getManagedDirName() { return getManagedElementName().toLowerCase(); }
   protected String getMediaName() { return mediaName; }
 
+  protected Element createTypeElement(String type) {
+    return editor.getElement(createXPathElement(), "Type", type);
+  }
+  protected void addMediaElement(String type, File file) {
+    Element e = editor.getElement(createTypeElement(type), getMediaName(), file.getName());
+    editor.addElement(e, "Name", null, file.getName().replaceAll("\\.[^.]*$", ""));
+    editor.addElement(e, "Src", null, "/"+getManagedDirName()+"/"+type+"/"+file.getName());
+    update(e);
+  }
+  protected void removeMediaElement(String type, File file) {
+    update(editor.setRemovePI(editor.getElement(createTypeElement(type), getMediaName(), file.getName())));
+  }
+
+  private Map<String, FileAlterationMonitor> typeMonitors = new Hashtable<String, FileAlterationMonitor>();
   private String mediaName;
 
   private String htmlDirName = ScoreBoardManager.getProperty(JettyServletScoreBoardController.PROPERTY_HTML_DIR_KEY);
+
+  protected class MediaTypeDirAlterationListener extends FileAlterationListenerAdaptor implements FileAlterationListener
+  {
+    public MediaTypeDirAlterationListener(String t) { type = t; }
+    public void onFileCreate(File f) { addMediaElement(type, f); }
+    public void onFileDelete(File f) { removeMediaElement(type, f); }
+    protected String type;
+  }
+
+  public static final String dbFileRegex = "^.*[.][dD][bB]$";
+  public static final String dotFileRegex = "^[.].*$";
+  public static final String invalidFileRegex = dbFileRegex+"|"+dotFileRegex;
+  public static final IOFileFilter mediaFileFilter = new AndFileFilter(FileFileFilter.FILE, new NotFileFilter(new RegexFileFilter(invalidFileRegex)));
+
+  public static final long INTERVAL = 1000; // in ms
 }
