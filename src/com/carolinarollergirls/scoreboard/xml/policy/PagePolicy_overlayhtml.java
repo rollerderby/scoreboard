6b8aebb399f0ba9398c5250052af02e74bebea69
Rob Thomas <xrobau@gmail.com>
2013-07-18 23:12:18 +1000

    Add an option for Team Logos to be displayed on the main overlay page.

---
+++ src/com/carolinarollergirls/scoreboard/xml/policy/PagePolicy_overlayhtml.java
@@ -0,0 +1,13 @@
+package com.carolinarollergirls.scoreboard.xml.policy;
+
+import com.carolinarollergirls.scoreboard.defaults.DefaultPolicyModel;
+
+public class PagePolicy_overlayhtml extends DefaultPolicyModel {
+  public PagePolicy_overlayhtml() {  super(ID, NAME, DESCRIPTION);  }
+
+  public static final String ID = "PagePolicy_overlay.html";
+  public static final String NAME = "Page: overlay.html";
+  public static final String DESCRIPTION = "When Enabled, the overlay page will display team logos next to their Time-Outs-Remaining area.";
+
+}
+
