f87195765a1d8c69082b440621377ab28e22fc01
Rob Thomas <xrobau@gmail.com>
2013-07-25 22:02:28 +1000

    Beginning of wftda.tv layout, for testing.

---
+++ html/views/wftdatv.js
@@ -0,0 +1,12 @@
+
+/**
+ * Copyright (C) 2008-2012 Mr Temper <MrTemper@CarolinaRollergirls.com>
+ * Copyright (C) 2013 Rob Thomas <xrobau@gmail.com>
+ *
+ * This file is part of the Carolina Rollergirls (CRG) ScoreBoard.
+ * The CRG ScoreBoard is licensed under either the GNU General Public
+ * License version 3 (or later), or the Apache License 2.0, at your option.
+ * See the file COPYING for details.
+ */
+
+XML_ELEMENT_SELECTOR = "ScoreBoard";
