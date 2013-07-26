5bfb87d4adf868454709b3a8176cf9d590928883
Rob Thomas <xrobau@gmail.com>
2013-07-27 08:51:02 +1000

    Just my messing around with the layout.

--- html/views/wftdatv.js
+++ html/views/wftdatv.js
@@ -10,3 +10,26 @@
  */
 
 XML_ELEMENT_SELECTOR = "ScoreBoard";
+
+function setupMainDiv(div) {
+	div.css({ position: "fixed" });
+
+	_crgUtils.bindAndRun($(window), "resize", function() {
+	  var aspect16x9 = _windowFunctions.get16x9Dimensions();
+	  div.css(aspect16x9).css("fontSize", aspect16x9.height/30); // Font scaling. 
+	  });
+} 
+
+$sb(function() {
+	  setupMainDiv($("#mainDiv"));
+
+	  console.log("Derp");
+	  // Team Names
+	  $.each( [ "1", "2" ], function(i, team) {
+		  $sb("ScoreBoard.Team("+team+").Name").$sbElement("#Team"+team+">.TeamName>a", {
+		      sbelement: { autoFitText: true }
+		    });
+	  });
+});
+
+	
