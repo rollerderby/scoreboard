480f746abcdd928a1e28eb4a0a1f1369e7c05ff2
ddstreet <ddstreet@ieee.org>
2013-09-24 19:01:11 -0400

    TABIFY: change spaces to tabs in model/* files.  Whitespace change only.

--- src/com/carolinarollergirls/scoreboard/model/ClockModel.java
+++ src/com/carolinarollergirls/scoreboard/model/ClockModel.java
@@ -12,35 +12,35 @@ import com.carolinarollergirls.scoreboard.*;
 
 public interface ClockModel extends Clock
 {
-  public ScoreBoardModel getScoreBoardModel();
+	public ScoreBoardModel getScoreBoardModel();
 
-  public Clock getClock();
+	public Clock getClock();
 
-  public void reset();
+	public void reset();
 
-  public void start();
-  public void stop();
+	public void start();
+	public void stop();
 
-  public void unstart();
-  public void unstop();
+	public void unstart();
+	public void unstop();
 
-  public void setName(String name);
+	public void setName(String name);
 
-  public void setNumber(int n);
-  public void changeNumber(int n);
-  public void setMinimumNumber(int n);
-  public void changeMinimumNumber(int n);
-  public void setMaximumNumber(int n);
-  public void changeMaximumNumber(int n);
+	public void setNumber(int n);
+	public void changeNumber(int n);
+	public void setMinimumNumber(int n);
+	public void changeMinimumNumber(int n);
+	public void setMaximumNumber(int n);
+	public void changeMaximumNumber(int n);
 
-  public void setTime(long ms);
-  public void changeTime(long ms);
-  public void resetTime();
-  public void setMinimumTime(long ms);
-  public void changeMinimumTime(long ms);
-  public void setMaximumTime(long ms);
-  public void changeMaximumTime(long ms);
+	public void setTime(long ms);
+	public void changeTime(long ms);
+	public void resetTime();
+	public void setMinimumTime(long ms);
+	public void changeMinimumTime(long ms);
+	public void setMaximumTime(long ms);
+	public void changeMaximumTime(long ms);
 
-  public void setCountDirectionDown(boolean down);
+	public void setCountDirectionDown(boolean down);
 }
 
