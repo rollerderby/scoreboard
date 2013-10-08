6627faa3d9e37f032cc06aceb4ffe20d68071323
Dan Streetman <ddstreet@ieee.org>
2013-10-08 15:22:17 -0400

    Remove most of media_management page complexity

--- html/controls/media_management.js
+++ html/controls/media_management.js
@@ -10,41 +10,11 @@
 
 
 $sb(function() {
-	$("#tabBar>button")
-		.filter(".Remove").click(function() {
-			createRemoveTypeDialog();
-		}).end()
-		.filter(".Add").click(function() {
-			createAddSpecificDialog();
-		}).end()
-		.filter(".Update").click(function() {
-			createUpdateMediaDialog();
-		}).end()
-		.button();
 
 	$("body>table.TypeTemplate")
 		.find("tr.Type>th.Type>button")
 		.filter(".Show,.Hide").click(function() {
 			$(this).closest("table").toggleClass("Hide");
-		}).end()
-		.filter(".ShowAvailable,.HideAvailable").click(function() {
-			$(this).closest("table").toggleClass("ShowAvailable");
-		}).end()
-		.end()
-		.find("tr.Controls>th.Remove>button.Remove").click(function() {
-			var table = $(this).closest("table");
-			table.data("sbType").children(table.data("childName")).each(function() {
-				$sb(this).$sbRemove();
-			});
-		}).end()
-		.find("tr.Controls>th.Preview>button.ShowPreview").click(function() {
-			$(this).closest("table.Type").find("tr.Item>td.Preview").addClass("Show");
-		}).end()
-		.find("tr.Controls>th.Preview>button.HidePreview").click(function() {
-			$(this).closest("table.Type").find("tr.Item>td.Preview").removeClass("Show");
-		}).end()
-		.find("tr>td.Preview>button.Preview").click(function() {
-			$(this).parent().addClass("Show");
 		}).end();
 
 	$.each( [ "Images", "Videos", "CustomHtml" ], function() {
@@ -73,12 +43,6 @@ function setupTab(parentName, childName, previewElement) {
 			})
 			.find("th.Type>button.Upload").click(function() {
 				createUploadMediaDialog($(this).closest("table"));
-			}).end()
-			.find("th.Type>button.RefreshAvailable,th.Type>button.ShowAvailable").click(function() {
-				refreshAvailable($(this).closest("table"));
-			}).end()
-			.find("th.Type>button.AddAllAvailable").click(function() {
-				$(this).closest("table").find("tr.Available>td.Add>button.Add").click();
 			}).end()
 			.find("thead button").button().end()
 			.find("tr.Type>th.Type>a.Type>span.Type").html(node.$sbId).end();
@@ -110,67 +74,17 @@ function setupTab(parentName, childName, previewElement) {
 					convert: function(val) { return srcprefix + String(val); }
 				}
 			});
-			node.$sb("Src").$sbElement(previewElement).click(function() { 
-				$(this).parent().removeClass("Show");
-			}).appendTo(newRow.find("td.Preview"));
+			node.$sb("Src").$sbElement(previewElement).appendTo(newRow.find("td.Preview"));
 			_windowFunctions.appendAlphaSortedByData(table.children("tbody"), newRow, "sbId", 2);
-			table.find("tr.Available").filter(function() { return $(this).data("sbId") == node.$sbId; }).remove();
 		},
 		remove: function(event,node) {
 			$("#"+parentName+">div.Type>table.Type")
 				.filter(function() { return $(this).data("type") == $sb(node.parent()).$sbId; })
-				.find("tr.Item:not(.Available)")
+				.find("tr.Item")
 				.filter(function() { return $(this).data("sbId") == node.$sbId; })
 				.remove();
 		}
 	});
-}
-
-function refreshAvailable(table) {
-	var parentName = table.data("parentName");
-	var childName = table.data("childName");
-	var previewElement = table.data("previewElement");
-	var media = parentName.toLowerCase();
-	var type = table.data("type");
-	var sbType = table.data("sbType");
-
-	// Disable the buttons while refreshing
-	table.find("th.Type>button.AvailableControl").button("disable");
-
-	$.get("/Media/list", { media: media, type: type })
-		.fail(function(jqxhr, textStatus, errorThrown) {
-			alert("Error getting available media : "+jqxhr.responseText);
-		})
-		.always(function() {
-			table.find("th.Type>button.AvailableControl").button("enable");
-		})
-		.done(function(data, status, jqxhr) {
-			table.find("tr.Available").remove();
-			var response = jqxhr.responseText.trim();
-			if (!response)
-				return;
-			$.each(response.split("\n"), function(i,e) {
-				var name = e.replace(/\.[^.]*$/, "");
-				var source = "/"+media+"/"+type+"/"+e;
-				if (sbType.children(childName).filter(function() { return $sb(this).$sbId == e; }).length)
-					return;
-				var newRow = table.find("tr.AvailableTemplate").clone(true)
-					.removeClass("AvailableTemplate").addClass("Available Item").data("sbId", e);
-				newRow.find("button").button()
-					.filter(".Add").click(function() {
-						var newElem = sbType.$sb(childName+"("+e+")");
-						newElem.$sb("Name").$sbSet(name);
-						newElem.$sb("Src").$sbSet(source);
-						$(this).button("option", "label", "Adding...").button("disable");
-					});
-				newRow.find("td.Name>a").text(name);
-				newRow.find("td.Src>a").text(e);
-				$(previewElement).attr("src", source).click(function() { 
-					$(this).parent().removeClass("Show");
-				}).appendTo(newRow.find("td.Preview"));
-				_windowFunctions.appendAlphaSortedByData(table.children("tbody"), newRow, "sbId", 2);
-			});
-		});
 }
 
 function createUploadMediaDialog(table) {
@@ -241,112 +155,5 @@ function createUploadMediaDialog(table) {
 			inputName.val("Cannot set name when uploading multiple files").prop("disabled", true);
 		}
 	}).change();
-}
-
-function createRemoveTypeDialog() {
-	return $("body>div.RemoveTypeDialog.DialogTemplate").clone(true)
-		.removeClass("DialogTemplate")
-		.dialog({
-			title: "Remove empty Type",
-			modal: true,
-			width: 700,
-			close: function() { $(this).dialog("destroy").remove(); },
-			buttons: {
-				Remove: function() {
-					var option = $(this).find("select.Media>option:selected");
-					var parentName = option.text();
-					var type = $(this).find("input:text.Type").val();
-					var sbType = $sb(parentName).children("Type[Id='"+type+"']");
-					if (sbType.length)
-						$sb(sbType).$sbRemove();
-					$(this).dialog("close");
-				},
-				Cancel: function() {
-					$(this).dialog("close");
-				}
-			}
-		});
-}
-
-function createAddSpecificDialog() {
-	return $("body>div.AddSpecificDialog.DialogTemplate").clone(true)
-		.removeClass("DialogTemplate")
-		.dialog({
-			title: "Add unavailable media",
-			modal: true,
-			width: 700,
-			close: function() { $(this).dialog("destroy").remove(); },
-			buttons: {
-				Add: function() {
-					var option = $(this).find("select.Media>option:selected");
-					var parentName = option.text();
-					var childName = option.val();
-					var type = $(this).find("input:text.Type").val();
-					var name = $(this).find("input:text.Name").val();
-					var filename = $(this).find("input:text.Filename").val();
-					var id = _crgUtils.checkSbId(filename);
-					var src = "/"+parentName.toLowerCase()+"/"+type+"/"+filename;
-					var newChild = $sb(parentName+".Type("+type+")."+childName+"("+id+")");
-					newChild.$sb("Name").$sbSet(name);
-					newChild.$sb("Src").$sbSet(src);
-					$(this).dialog("close");
-				},
-				Cancel: function() {
-					$(this).dialog("close");
-				}
-			}
-		});
-}
-
-function createUpdateMediaDialog() {
-	var div = $("body>div.UpdateMediaDialog.DialogTemplate").clone(true)
-		.removeClass("DialogTemplate");
-	var packages = [
-		{ media: "videos", type: "fullscreen" },
-		{ media: "images", type: "fullscreen" },
-		{ media: "images", type: "teamlogo" }
-	];
-	$.each(packages, function(i,pkg) {
-		var p = div.find("p.Template").clone(true)
-			.removeClass("Template").prependTo(div);
-		p.find("a.Name").text("crg-scoreboard-"+pkg.media+"-"+pkg.type);
-		var updateVersions = function() {
-			var localD = $.Deferred(), latestD = $.Deferred();
-			$.get("/Media/localversion", pkg)
-				.done(function(level) { p.children("a.LocalVersion").text(level); })
-				.fail(function() { p.children("a.LocalVersion").text("Unknown"); })
-				.always(function() { localD.resolve(); });
-			$.get("/Media/latestversion", pkg)
-				.done(function(level) { p.children("a.LatestVersion").text(level); })
-				.fail(function() { p.children("a.LatestVersion").text("Unknown"); })
-				.always(function() { latestD.resolve(); });
-			$.when(localD, latestD).always(function() {
-				var localVersion = p.children("a.LocalVersion").text();
-				var latestVersion = p.children("a.LatestVersion").text();
-				//FIXME - use isNumeric here after moving to jquery 1.7
-				if (isNaN(latestVersion))
-					p.children("button.Update").button("option", "label", "Unknown Latest Version").button("disable");
-				else if (isNaN(localVersion) || (localVersion < latestVersion))
-					p.children("button.Update").button("option", "label", "Update To Latest Version").button("enable");
-				else
-					p.children("button.Update").button("option", "label", "Install Latest Version Again").button("enable");
-			});
-		};
-		p.children("button.Update").click(function() {
-			$(this).button("option", "label", "Updating...").button("disable");
-			$.get("/Media/update", pkg)
-				.done(function(results) { div.find(">p.Results>a.Results").text(results); })
-				.fail(function(jqxhr, textStatus, errorThrown) { div.find(">p.Results>a.Results").text("Failed: "+textStatus); })
-				.always(function() { updateVersions(); });
-		}).button({ label: "Checking Version...", disabled: true });
-		updateVersions();
-	});
-	div.dialog({
-		title: "Update media",
-		modal: true,
-		width: 500,
-		close: function() { $(this).dialog("destroy").remove(); },
-		buttons: { Close: function() { $(this).dialog("close"); } }
-	});
 }
 
