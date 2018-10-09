ed550499a80aaf9936ebaabe40232266eac67d92
Brian Brazil <brian.brazil@robustperception.io>
2018-10-09 19:16:21 +0100

    Specify cache-control headers for static content (#181)
    no-cache means the browser will check with the server on each load,
    and the etags help ensure it'll only pull down the full file if needed.
    
    This should eliminate issues around users with old JS loaded.
    
    Fixes #63

--- src/com/carolinarollergirls/scoreboard/jetty/JettyServletScoreBoardController.java
+++ src/com/carolinarollergirls/scoreboard/jetty/JettyServletScoreBoardController.java
@@ -121,7 +121,8 @@ public class JettyServletScoreBoardController
 		if (null != staticPath) {
 			ServletContextHandler c = new ServletContextHandler(contexts, "/");
 			ServletHolder sh = new ServletHolder(new DefaultServlet());
-			sh.setInitParameter("org.eclipse.jetty.servlet.Default.cacheControl", "no-cache");
+			sh.setInitParameter("cacheControl", "no-cache");
+			sh.setInitParameter("etags", "true");
 			c.addServlet(sh, "/*");
 			c.addFilter(mf, "/*", 1);
 			c.setResourceBase((new File(ScoreBoardManager.getDefaultPath(), staticPath)).getPath());
