0c917ab46b57c61b5fdbb8f3c8304a9c2a00337c
Brian Brazil <brian.brazil@robustperception.io>
2019-04-13 11:31:54 +0100

    Don't head before adding js/css. (#230)
    It still shows up as an error in the Console either way,
    so may aswell simplify.

--- html/json/core.js
+++ html/json/core.js
@@ -16,9 +16,6 @@ if (typeof $ == "undefined") {
 
 function _includeUrl(url) {
 	var filename = url.replace(/^.*[\/]/g, "");
-	/* Use HTTP HEAD to verify url exists before adding it to the document */
-	if ($.ajax(url, { async: false, type: "HEAD", global: false }).status != 200)
-		return;
 	if (/\.[cC][sS][sS](\?.*)?$/.test(url) && !$("head link[href='"+url+"'],head link[href='"+filename+"']").length)
 		$("<link>").attr({ href: url, type: "text/css", rel: "stylesheet"}).appendTo("head");
 	else if (/\.[jJ][sS](\?.*)?$/.test(url) && !$("head script[src='"+url+"'],head script[src='"+filename+"']").length)
