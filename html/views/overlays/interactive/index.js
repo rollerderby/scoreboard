e62be5e8f412f87b7e552e265252b23168906c7f
Brian Brazil <brian.brazil@robustperception.io>
2019-06-03 16:42:49 +0100

    Fix and reorganize overlay code.
    Correctly handle skaters changing number, skaters
    with no name, skaters with no number, ignore foexp (for now),
    count penalties correctly when colouring, handle null logo.
    
    Break down big functions into smaller more understandable parts,
    and use the new WS functionality to simplify.

--- html/views/overlays/interactive/index.js
+++ html/views/overlays/interactive/index.js
@@ -1,38 +1,100 @@
 
-function dec_sort(a, b){ return ( parseInt($(b).attr("data-sort")) < parseInt($(a).attr("data-sort")) ) ? 1 : -1; }
-function str_sort(a, b){ return ( $(b).attr("data-sort") < $(a).attr("data-sort") ) ? 1 : -1; }
+function str_sort(a, b){ return ( $(a).attr("data-sort") == null ||	$(b).attr("data-sort") < $(a).attr("data-sort") ) ? 1 : -1; }
 
 jQuery.fn.sortDivs = function sortDivsStr() { $("> div", this[0]).sort(str_sort).appendTo(this[0]); }
-jQuery.fn.sortDivsRev = function sortDivsRev() { $("> div", this[0]).sort(dec_sort).appendTo(this[0]); }
 
 $(initialize);
 
 function initialize() {
 
-	WS.Connect();
-	WS.AutoRegister();
-
 	WS.Register( [ "ScoreBoard.Clock(Intermission).Number",
-		       "ScoreBoard.Rulesets.CurrentRule(Period.Number)",
-		       "ScoreBoard.Settings.Setting(ScoreBoard.Intermission.PreGame)",
-		       "ScoreBoard.Settings.Setting(ScoreBoard.Intermission.Unofficial)",
-		       "ScoreBoard.Settings.Setting(ScoreBoard.Intermission.Official)",
-		       "ScoreBoard.Settings.Setting(ScoreBoard.Intermission.Intermission)" ], function(k,v) { } );
+			"ScoreBoard.Rulesets.CurrentRule(Period.Number)",
+			"ScoreBoard.Settings.Setting(ScoreBoard.Intermission.PreGame)",
+			"ScoreBoard.Settings.Setting(ScoreBoard.Intermission.Unofficial)",
+			"ScoreBoard.Settings.Setting(ScoreBoard.Intermission.Official)",
+			"ScoreBoard.Settings.Setting(ScoreBoard.Intermission.Intermission)" ]);
 
-	WS.Register( [  'ScoreBoard.Clock(Timeout).Running', 
-		        'ScoreBoard.TimeoutOwner',
+	WS.Register( [	'ScoreBoard.Clock(Timeout).Running',
+			'ScoreBoard.TimeoutOwner',
 			'ScoreBoard.OfficialReview',
-			'ScoreBoard.Team(1).Timeouts',
-			'ScoreBoard.Team(2).Timeouts',
-			'ScoreBoard.Team(1).OfficialReviews',
-			'ScoreBoard.Team(2).OfficialReviews',
-			'ScoreBoard.Team(1).RetainedOfficialReview',
-			'ScoreBoard.Team(2).RetainedOfficialReview' ], function(k,v) { smallDescriptionUpdate(k,v); } );
+			'ScoreBoard.Team(*).Timeouts',
+			'ScoreBoard.Team(*).OfficialReviews',
+			'ScoreBoard.Team(*).RetainedOfficialReview' ], function(k,v) { smallDescriptionUpdate(k,v); } );
 
-	WS.Register( 'ScoreBoard.Period', function(k,v) { jamData(k,v); } );
+	WS.Register( ['ScoreBoard.Period(*).Jam(*).TeamJam(*).JamScore',
+			'ScoreBoard.Period(*).Jam(*).TeamJam(*).Lead',
+			'ScoreBoard.Period(*).Jam(*).TeamJam(*).Lost'], function(k,v) { jamData(k,v); } );
 
-	WS.Register( 'ScoreBoard.Team(1)', function(k,v) { teamData(1, k,v); } );
-	WS.Register( 'ScoreBoard.Team(2)', function(k,v) { teamData(2, k,v); } );
+	WS.Register( [
+			'ScoreBoard.Team(*).Skater(*).Name',
+			'ScoreBoard.Team(*).Skater(*).Number',
+			'ScoreBoard.Team(*).Skater(*).Flags' ], function(k,v) {
+				var me = '.RosterTeam' + k.Team + ' .Team' + k.Team + ' .Skater[data-skaterId=' + k.Skater + ']';
+				var mb = '.PenaltyTeam' + k.Team + ' .Team' + k.Team + ' .Skater[data-skaterId=' + k.Skater + ']';
+				if (v == null) {
+					$(me).remove();
+					$(mb).remove();
+					return;
+				}
+				ensureSkaterExists(k.Skater, k.Team);
+
+				if(k.field == 'Number')	{ updateSkaterNumber(me,mb,v); }
+				if(k.field == 'Flags') {
+					$('.'+k.field, me).attr('data-flag', v);
+					$(mb).attr('data-flag', v);
+				} else if (v == "") {
+					c = $('.'+k.field, me).html('&nbsp;');
+					c = $('.'+k.field, mb).html('&nbsp;');
+				} else {
+					c = $('.'+k.field, me).text(v);
+					c = $('.'+k.field, mb).text(v);
+				}
+			});
+
+	WS.Register( 'ScoreBoard.Team(*).Skater(*).Penalty(*).Code', function(k,v) {
+		if (k.Penalty == 0) {
+			// Foulout/Expulsion.
+			return;
+		}
+		var sel = '.PenaltyTeam' + k.Team + ' .Team' + k.Team + ' .Skater[data-skaterId=' + k.Skater + ']';
+		if (v == null) {
+			$('.Number-'+k.Penalty, sel).remove();
+			$(sel).attr('data-count', $('.Penalty', sel).length);
+			return;
+		}
+		ensureSkaterExists(k.Skater, k.Team);
+		createPenalty(sel, k.Penalty, v);
+	} );
+
+	WS.Register( ['ScoreBoard.Team(*).Color(overlay_bg)',
+			'ScoreBoard.Team(*).Color(overlay_fg)' ], function(k,v) {
+		var style;
+		for(i in document.styleSheets) if(document.styleSheets[i].title =='jsStyle') style=document.styleSheets[i];
+		if(style) {
+			var ns,r;
+			var rule;
+			// chrome seems to like things in lowercase
+			var rd = '#sb .colourteam'+k.Team;
+
+			if(k.Color == 'overlay_bg') ns = 'background-color';
+			if(k.Color == 'overlay_fg') ns = 'color';
+			for(var r=0; r<style.rules.length ; r++ ) {
+				var dd = style.rules[r];
+				if(dd.selectorText == rd && dd.style[0] == ns) style.deleteRule(r);
+			}
+			if(v != null) style.addRule(rd, ns + ': ' + v);
+		}
+	});
+
+	WS.Register( 'ScoreBoard.Team(*).Logo', function(k,v) {
+		if(v && v != '') {
+			$('img.TeamLogo'+k.Team).attr('src', v).css('display', 'block');
+			$('img.TeamLogo'+k.Team).parent().removeClass('NoLogo');
+		} else {
+			$('img.TeamLogo'+k.Team).css('display', 'none');
+			$('img.TeamLogo'+k.Team).parent().addClass('NoLogo');
+		}
+	});
 
 	WS.Register( 'ScoreBoard.Clock(Period).Number', function(k,v) {
 		if(v == 2) { $('.PPJBox .Team .Period2').show(); } else { $('.PPJBox .Team .Period2').hide(); }
@@ -44,31 +106,28 @@ function initialize() {
 		}
 	});
 
-	WS.Register([ 'ScoreBoard.Settings.Setting(Overlay.Interactive.Clock)', 'ScoreBoard.Settings.Setting(Overlay.Interactive.Score)' ], function(k,v) {  
+	WS.Register([ 'ScoreBoard.Settings.Setting(Overlay.Interactive.Clock)', 'ScoreBoard.Settings.Setting(Overlay.Interactive.Score)' ], function(k,v) {
 		$('div[data-setting="'+k+'"]').each(function() {
 			if(v == 'On') { $(this).addClass('Show'); } else { $(this).removeClass('Show'); }
 		});
 	});
 
-	WS.Register([ 'ScoreBoard.Settings.Setting(Overlay.Interactive.ShowJammers)' ], function(k,v) {  
+	WS.Register([ 'ScoreBoard.Settings.Setting(Overlay.Interactive.ShowJammers)' ], function(k,v) {
 		$('div[data-setting="'+k+'"]').each(function() {
 			if(v == 'On') { $(this).addClass('ShowJammers'); } else { $(this).removeClass('ShowJammers'); }
 		});
 	});
 
-	WS.Register('ScoreBoard.Settings.Setting(Overlay.Interactive.Transition)', function(k,v) { 
-		window.alert(k,v);
-	});
 
-	WS.Register('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', function(k,v) { 
-		$('.OverlayPanel').removeClass('Show'); 
+	WS.Register('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', function(k,v) {
+		$('.OverlayPanel').removeClass('Show');
 		// sort divs in the panel before we show, just in case it's changed
 		if(v == 'PenaltyTeam1' || v == 'PenaltyTeam2') {
 			c = $('.PenaltyTeam [data-flag="BC"]');
 			c.empty().remove();
 		}
-		$('.OverlayPanel.'+v+' .SortBox').sortDivs(); 
-		$('.OverlayPanel.' + v).addClass('Show'); 
+		$('.OverlayPanel.'+v+' .SortBox').sortDivs();
+		$('.OverlayPanel.' + v).addClass('Show');
 	});
 
 	WS.Register([ 'ScoreBoard.Settings.Setting(Overlay.Interactive.LowerThird.Line' ] , function(k,v) {
@@ -76,145 +135,56 @@ function initialize() {
 		$(sp).text(v);
 	});
 
-	WS.Register([ 'ScoreBoard.Settings.Setting(Overlay.Interactive.LowerThird.Style)' ] , function(k,v) { 
-		$('.LowerThird .Line2').removeClass( 'ColourTeam1 ColourTeam2 ColourDefault' ).addClass(v);
-	});
+			WS.Register([ 'ScoreBoard.Settings.Setting(Overlay.Interactive.LowerThird.Style)' ] , function(k,v) {
+				$('.LowerThird .Line2').removeClass( 'ColourTeam1 ColourTeam2 ColourDefault' ).addClass(v);
+			});
 
-	$(document).keyup(function(e) {
-		if(e.which == 74) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.ShowJammers)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.ShowJammers)'] == 'On' ? 'Off' : 'On'); }
-		if(e.which == 67) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Clock)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Clock)'] == 'On' ? 'Off' : 'On'); }
-		if(e.which == 83) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Score)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Score)'] == 'On' ? 'Off' : 'On'); }
-		if(e.which == 49) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)'] == 'RosterTeam1' ? '' : 'RosterTeam1'); }
-		if(e.which == 50) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)'] == 'RosterTeam2' ? '' : 'RosterTeam2'); }
-		if(e.which == 51) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)'] == 'PenaltyTeam1' ? '' : 'PenaltyTeam1'); }
-		if(e.which == 52) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)'] == 'PenaltyTeam2' ? '' : 'PenaltyTeam2'); }
-		if(e.which == 32) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', ''); }
-	});
+			$(document).keyup(function(e) {
+				if(e.which == 74) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.ShowJammers)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.ShowJammers)'] == 'On' ? 'Off' : 'On'); }
+				if(e.which == 67) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Clock)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Clock)'] == 'On' ? 'Off' : 'On'); }
+				if(e.which == 83) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Score)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Score)'] == 'On' ? 'Off' : 'On'); }
+				if(e.which == 49) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)'] == 'RosterTeam1' ? '' : 'RosterTeam1'); }
+				if(e.which == 50) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)'] == 'RosterTeam2' ? '' : 'RosterTeam2'); }
+				if(e.which == 51) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)'] == 'PenaltyTeam1' ? '' : 'PenaltyTeam1'); }
+				if(e.which == 52) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', WS.state['ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)'] == 'PenaltyTeam2' ? '' : 'PenaltyTeam2'); }
+				if(e.which == 32) { WS.Set('ScoreBoard.Settings.Setting(Overlay.Interactive.Panel)', ''); }
+			});
 
-	setTimeout(function() { $('body').removeClass('preload'); }, 1000);                                                                                                                                            
+			WS.AutoRegister();
+			WS.Connect();
 
+			setTimeout(function() { $('body').removeClass('preload'); }, 1000);
 }
 
 
-function teamData(team, k,v) {
 
-	var skaterId;
-	var key;
-	var setting; 
+function ensureSkaterExists(skaterId, team) {
+	if ($('.PenaltyTeam' + team + ' .Team' + team + ' .Skater[data-skaterId=' + skaterId + ']').length == 0) {
+		// create the roster entry for this skater
+		var xv = $('<div class="Skater"></div>');
+		xv.attr('data-skaterId', skaterId);
+		$('<div class="Number ColourTeam' + team +'"></div>').appendTo(xv);
+		$('<div class="Name">&nbsp;</div>').appendTo(xv);
+		$('<div class="Flags">&nbsp;</div>').appendTo(xv);
+		$('.RosterTeam' + team + ' .Team' + team).append(xv);
 
-	var skaterRegEx = /^ScoreBoard\.Team\((.+)\)\.Skater\((.+)\)\.(.+)$/;
-	var match = k.match(skaterRegEx);
-	if(match) { 
-		skaterId = match[2]; key = match[3]; penalty = null; 
+		// create the penalty tracking entry for this skater
+		var xz = $('<div class="Skater"></div>');
+		xz.attr('data-skaterId', skaterId);
+		xz.attr('data-count', 0);
+		$('<div class="Number ColourTeam' + team + '">&nbsp;</div>').appendTo(xz);
+		$('<div class="Name">&nbsp;</div>').appendTo(xz);
+		$('<div class="Penalties">&nbsp;</div>').appendTo(xz);
+		$('.PenaltyTeam' + team + ' .Team' + team).append(xz);
 	}
-
-	var penaltyRegEx = /^ScoreBoard\.Team\((.+)\)\.Skater\((.+)\)\.Penalty\((.+)\).(.+)$/;
-	var match = k.match(penaltyRegEx);
-	if(match) { skaterId = match[2]; key = match[4]; penalty = match[3]; }
-
-	var teamInfoRegEx = /^ScoreBoard\.Team\((.+)\)\.(.+)$/;
-	var match = k.match(teamInfoRegEx);
-	if(match) { 
-		var subkey = match[2];
-		if(subkey == 'Logo') { 
-			if(v && v != '') {
-				$('img.TeamLogo'+team).attr('src', v).css('display', 'block');
-				$('img.TeamLogo'+team).parent().removeClass('NoLogo');
-			} else {
-				$('img.TeamLogo'+team).css('display', 'none');
-				$('img.TeamLogo'+team).parent().addClass('NoLogo');
-			}
-		}
-	}
-
-	var colourRegEx = /^ScoreBoard\.Team\((.+)\)\.Color\((.+)\)$/;
-	var match = k.match(colourRegEx);
-	if(match) { 
-		var setting = match[2];
-		var style;
-		for(i in document.styleSheets) if(document.styleSheets[i].title =='jsStyle') style=document.styleSheets[i];
-		if(style) {
-			var ns,r;
-			var rule;
-			// chrome seems to like things in lowercase
-			var rd = '#sb .colourteam'+team;
-
-			if(setting == 'overlay_bg') ns = 'background-color';
-			if(setting == 'overlay_fg') ns = 'color';
-			if(ns) {
-				for(var r=0; r<style.rules.length ; r++ ) {
-					var dd = style.rules[r];
-					if(dd.selectorText == rd && dd.style[0] == ns) style.deleteRule(r);
-				}
-				if(v != null) style.addRule(rd, ns + ': ' + v);
-			}
-		}
-		return;
-	}
-
-	if(key != 'Name' && key != 'Number' && key != 'Code' && key != 'Flags')  { return; }
-
-	var pa = '.RosterTeam' + team + ' .Team' + team;
-	var pb = '.PenaltyTeam' + team + ' .Team' + team;
-
-	var me = pa + ' .Skater[data-skaterId=' + skaterId + ']';
-	var mb = pb + ' .Skater[data-skaterId=' + skaterId + ']';
-
-	if (v == null) {
-		if(key == 'Code') {
-			$('.Number-'+penalty, mb).remove();
-		} else {
-			$(mb).remove();
-			$(me).remove();
-		}
-		return;
-	}
-
-
-	if($(me).length == 0)   { createRosterSkater(pa, skaterId, team); }
-	if($(mb).length == 0) 	{ createPenaltySkater(pb, skaterId, team); }
-
-	if(key == 'Code') 	{ createPenalty(mb,penalty,v); } 
-	if(key == 'Number') 	{ updateSkaterNumber(me,mb,key,v); }
-
-	if(key == 'Flags') {
-		$('.'+key, me).attr('data-flag', v);
-		$(mb).attr('data-flag', v);
-	} else {	
-		c = $('.'+key, me).text(v);
-		c = $('.'+key, mb).text(v);
-	}
-
-
 }
 
-function updateSkaterNumber(me,mb,key,v) {
+function updateSkaterNumber(me,mb,v) {
 	sv = v;
-	if(v == '' || v == '-' || v == null) { sv = 'ZZZB'; v = '-'; }	
-	$('.'+key,me).parent().attr('data-sort', sv);
-	$('.'+key,mb).parent().attr('data-sort', sv);
-	$(me).sortDivs(); $(mb).sortDivs();
-}
-
-
-function createRosterSkater(pa, skaterId, team) {
-	// create the roster entry for this skater
-	var xv = $('<div class="Skater"></div>');
-	xv.attr('data-skaterId', skaterId);
-	$('<div class="Number ColourTeam' + team +'"></div>').appendTo(xv);
-	$('<div class="Name"></div>').appendTo(xv);
-	$('<div class="Flags"></div>').appendTo(xv);
-	$(pa).append(xv);
-}
-
-function createPenaltySkater(pb, skaterId, team) {
-	// create the penalty box entry for this skater
-	var xz = $('<div class="Skater"></div>');
-	xz.attr('data-skaterId', skaterId);
-	xz.attr('data-count', 0);
-	$('<div class="Number ColourTeam' + team + '">&nbsp;</div>').appendTo(xz);
-	$('<div class="Name">&nbsp;</div>').appendTo(xz);
-	$(pb).append(xz);
+	if(v == '' || v == '-' || v == null) { sv = 'ZZZB'; v = '-'; }
+	$('.Number',me).parent().attr('data-sort', sv);
+	$('.Number',mb).parent().attr('data-sort', sv);
+	$(me).parent().sortDivs(); $(mb).parent().sortDivs();
 }
 
 function createPenalty(mb, pnum, v) {
@@ -222,26 +192,20 @@ function createPenalty(mb, pnum, v) {
 	var penalty = $('<div class="Penalty Number-' + pnum + ' Penalty-' + v + '">' + v + '</div>');
 	$(mb).attr('data-count', $('.Penalty', mb).length+1);
 	penalty.attr('data-sort', pnum);
-	$(mb).append(penalty);
-	$(mb).sortDivs();
+	$(mb).children(".Penalties").append(penalty);
+	$(mb).children(".Penalties").sortDivs();
 }
 
-var jamScoreRegEx = /^ScoreBoard.Period\((.+)\)\.Jam\((.+)\)\.TeamJam\((.+)\)\.(.+)$/;
-
 function jamData(k,v) {
-        match = k.match(jamScoreRegEx);
-        if (match == null) return;
-
-        var period = match[1];
-        var jam = match[2];
-        var team = match[3];
-	var key = match[4];
-
-	if (key != 'JamScore' && key != 'DisplayLead') return;
+	var period = k.Period;
+	var jam = k.Jam;
+	var team = k.TeamJam;
+	var key = k.field;
 
 	pa = '.PPJBox .Team'+ team + ' .Period'+period;
 	me = pa + ' .Jam'+jam;
-	$pId = $(pa); $mId = $(me)
+	var $pId = $(pa);
+	var $mId = $(me);
 
 	if(v == null) {
 		$(me).remove();
@@ -258,29 +222,33 @@ function jamData(k,v) {
 		$pId.sortDivs();
 	}
 
-	if(key == 'DisplayLead') {
-		$(me).attr('lead', v);
+	switch(key) {
+		case 'Lead':
+			$(me).attr('lead', v);
+			break;
+		case 'Lost':
+			$(me).attr('lost', v);
+			break;
+		case 'JamScore':
+			setHeight = v*4 + 'px';
+			$(me).css('height', setHeight);
+
+			if(team == 1) {
+				hid = $('.PPJBox .Team1 .Period').innerHeight();
+				marg = parseInt(hid)-parseInt(setHeight);
+				$(me).css('marginTop', marg);
+			}
+			if(v != 0) { $('.Points', me).text(v); }
+			break;
 	}
 
-	if(key == 'JamScore') {
-		setHeight = v*4 + 'px';
-		$(me).css('height', setHeight);
-
-		if(team == 1) {
-			hid = $('.PPJBox .Team1 .Period').innerHeight();
-			marg = parseInt(hid)-parseInt(setHeight);
-			$(me).css('marginTop', marg);
-		}
-		if(v != 0) { $('.Points', me).text(v); }
-	}
 	pointsPerJamColumnWidths();
-
 }
 
 function pointsPerJamColumnWidths() {
 	ne1 = $('.PPJBox .Team1 .GraphBlock').length;
 	ne2 = $('.PPJBox .Team2 .GraphBlock').length;
-	if(ne2 > ne1)  ne1=ne2;
+	if(ne2 > ne1)	ne1=ne2;
 	nel = ne1 + 3;
 	wid = parseInt( $('.PPJBox').innerWidth() );
 	newwidth = parseInt(wid / nel) - 3;
@@ -310,7 +278,7 @@ function clockType(k,v) {
 		var num = WS.state["ScoreBoard.Clock(Intermission).Number"];
 		var max = WS.state["ScoreBoard.Rulesets.CurrentRule(Period.Number)"];
 		var isOfficial = WS.state["ScoreBoard.OfficialScore"];
-		if (num == 0)  
+		if (num == 0)
 			ret = WS.state["ScoreBoard.Settings.Setting(ScoreBoard.Intermission.PreGame)"];
 		else if (num != max)
 			ret = WS.state["ScoreBoard.Settings.Setting(ScoreBoard.Intermission.Intermission)"];
@@ -325,13 +293,7 @@ function clockType(k,v) {
 	}
 
 	return ret;
-	
+
 }
-
-
-
-$(document).ready(function() {
-	$('#sb').css('opacity',1);
-});
 
 //# sourceURL=views\overlays\interactive\index.js
