1965fa8ebf77f4243f87f9175fe37a335675c613
Frank Weinberg <github@usenet-rundfahrt.de>
2019-07-26 21:33:18 +0200

    review comment

--- tests/com/carolinarollergirls/scoreboard/event/TestNumberedScoreBoardEventProviderImpl.java
+++ tests/com/carolinarollergirls/scoreboard/event/TestNumberedScoreBoardEventProviderImpl.java
@@ -6,12 +6,7 @@ public class TestNumberedScoreBoardEventProviderImpl
     public TestNumberedScoreBoardEventProviderImpl(TestScoreBoardEventProvider parent, int number) {
         super(parent, number, TestScoreBoardEventProvider.NChild.NUMBERED,
                 TestNumberedScoreBoardEventProvider.class, Value.class, Child.class);
-        if (parent instanceof TestNumberedScoreBoardEventProvider) {
-            setCopy(Value.RO_DIRECT_COPY, parent, TestNumberedScoreBoardEventProvider.Value.INT, true);
-            setCopy(Value.RW_DIRECT_COPY, parent, TestNumberedScoreBoardEventProvider.Value.INT, false);
-        } else {
-            setCopy(Value.RO_DIRECT_COPY, parent, TestScoreBoardEventProvider.Value.INT, true);
-            setCopy(Value.RW_DIRECT_COPY, parent, TestScoreBoardEventProvider.Value.INT, false);
-        }
+        setCopy(Value.RO_DIRECT_COPY, parent, TestScoreBoardEventProvider.Value.INT, true);
+        setCopy(Value.RW_DIRECT_COPY, parent, TestScoreBoardEventProvider.Value.INT, false);
     }
 }
