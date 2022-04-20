d50e3fef582a569d19be55a64467dd902b4c628e
Frank Weinberg <github@usenet-rundfahrt.de>
2022-04-20 19:26:38 +0200

    Add "whiteboard" alternate name & use it

--- html/views/wb/index.js
+++ html/views/wb/index.js
@@ -3,8 +3,8 @@
   var gameId = _windowFunctions.getParam('game');
 
   setupGameAdvance($('#gameAdvance'), gameId, true);
-  preparePltInputTable($('#pt1'), gameId, '1', 'pt', null, 'scoreboard');
-  preparePltInputTable($('#pt2'), gameId, '2', 'pt', null, 'scoreboard');
+  preparePltInputTable($('#pt1'), gameId, '1', 'pt', null, 'whiteboard');
+  preparePltInputTable($('#pt2'), gameId, '2', 'pt', null, 'whiteboard');
 
   WS.AutoRegister();
   WS.Connect();
